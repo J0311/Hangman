527c87e331121991bf968c5db490c7a50b25bc89
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-05-13 18:53:12 -0400

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -94,38 +94,39 @@ public class Hangman {
 	}
 
 	private static void printHangedMan(Integer wrongCount) {
-		System.out.println("-------");
-		System.out.println("|     |");
-		
-		if (wrongCount >= 1) {
-			System.out.println(" O");
-		}
-		
-		if (wrongCount >= 2) {
-			System.out.print("\\ ");
-			if (wrongCount >= 3) {
-				System.out.print("/");
-		}
-			else {
-				System.out.println("");
-			}
-		}
-		
-		if (wrongCount >= 4) {
-			System.out.println(" |");
-		}
-		if (wrongCount >= 5) {
-			System.out.println("/ ");
-			if(wrongCount >= 6) {
-				System.out.println("\\");
-			}
-			else {
-				System.out.println("");
-			}
-		}
-		System.out.println("");
-		System.out.println("");
-	}
+	    System.out.println(" -------");
+	    System.out.println(" |     |");
+	    if (wrongCount >= 1) {
+	      System.out.println(" O");
+	    }
+	    
+	    if (wrongCount >= 2) {
+	      System.out.print("\\ ");
+	      if (wrongCount >= 3) {
+	        System.out.println("/");
+	      }
+	      else {
+	        System.out.println("");
+	      }
+	    }
+	    
+	    if (wrongCount >= 4) {
+	      System.out.println(" |");
+	    }
+	    
+	    if (wrongCount >= 5) {
+	      System.out.print("/ ");
+	      if (wrongCount >= 6) {
+	        System.out.println("\\");
+	      }
+	      else {
+	        System.out.println("");
+	      }
+	    }
+	    System.out.println("");
+	    System.out.println("");
+	  }
+
 			
 // Method to get player's guess.		
 // letterGuess will serve as variable to hold user input. To proof the parsing of only 
